ee658ecab5f3361ba0935c65844b5edfe4cdb7a5
Luke Carroll <luke.c.carroll92@gmail.com>
2018-09-04 09:59:01 +0100

    Rewrote two scripts.

--- GUI_contourf.py
+++ GUI_contourf.py
@@ -1,24 +1,15 @@
 import lidar
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.colors as colors
-from matplotlib.colors import BoundaryNorm
-from matplotlib.ticker import MaxNLocator
 from matplotlib.widgets import Button
+from matplotlib import colors, ticker, cm
+from matplotlib import dates
 import matplotlib
-import matplotlib.tri as mtri
-from datetime import datetime, timedelta
 
 lidar_data=lidar.lidar('metoffice-lidar_faam_20150807_r0_B920_raw.nc')
 
-
-
-
-time_epoch = lidar_data['Time'][:].data
-epoch_list = time_epoch.tolist()
-datetime_list = [datetime.utcfromtimestamp(epoch_list[i]) for i in range(0, len(epoch_list))]
-datetime_array = np.array(datetime_list)
-
+m_time = dates.epoch2num(lidar_data['Time'][:].data)
+full_altitude = lidar_data['Altitude (m)'][:].data
 height_correction = 1.5 * np.arange(12148)
 
 def z_maker(x,y):
@@ -32,7 +23,14 @@ def height_maker(x,y,z):
     altitude_array = altitude_array.clip(0)
     return altitude_array
 
-
+def time_maker(x,y,z):
+    time = lidar_data['Time'][x:y].data
+    time_array = np.empty_like(z)
+    for j in range(0,len(z)):
+        for i in range(0, len(z[j])):
+            time_array[j,i] = time[i]
+    mpl_time = dates.epoch2num(time_array)
+    return mpl_time
 
 full_z = z_maker(0, len(lidar_data.profile[0][:].data[0]))
 
@@ -41,46 +39,29 @@ full_height = height_maker(0, len(lidar_data.profile[0][:].data[0]), full_z)
 def height_quick_maker(x,y):
     return full_height[:, x:y]
 
-def time_maker(x,y,z):
-    time = lidar_data['Time'][x:y].data
-    time_array = np.empty_like(z)
-    for j in range(0,len(z)):
-        for i in range(0, len(z[j])):
-            time_array[j,i] = time[i]
-    return time_array
-
-
-fig, ax = plt.subplots()
-plt.subplots_adjust(bottom=0.2)
-
 fig, ax = plt.subplots()
 plt.subplots_adjust(bottom=0.2)
 start = 2000
 end = 2200
-z_test = z_maker(start, end)
-time_test = datetime_array[start:end]
-time_long = time_maker(start, end, z_test)
-# time_base = datetime_array[start:end]
-# base_list = time_base.tolist()
-height_test = height_quick_maker(start, end)
-width = end - start
-cont = plt.contour(time_long,height_test,z_test)
-#print(z_test.max())
-#plt.colorbar(c)
-
-
-
+z = z_maker(start, end)
+time = time_maker(start, end, z)
+time_flat = m_time[start:end]
+altitude = full_altitude[start:end]
+height = height_quick_maker(start, end)
+plt.ylim(0, altitude.max() * 1.1)
+plt.ylabel('Height (m)')
+plt.xlabel('time')
+contour_p = plt.contour(time,height,z, locator=ticker.LogLocator())
+line_p = plt.plot(time_flat, altitude, color='black', linewidth=2)
+myFmt = dates.DateFormatter('%H:%M')
+ax.xaxis.set_major_formatter(myFmt)
+plt.colorbar(contour_p)
 
 class Index(object):
     def next(self, event):
-        global cont
         print(start)
-        print(cont.get_array())
-        for c in cont.collections:
-            c.remove()
-        cont = plt.contour(time_long, height_test, z_test)
-        plt.draw()
-            # start = 2000
+        print(c.get_array())
+        # start = 2000
         # end = 2200
         # start += 100
         # end += 100
@@ -92,9 +73,7 @@ class Index(object):
         # plt.draw()
 
     def prev(self, event):
-        print("pooop")
-
-
+        print("Testing testing 123")
 
 callback = Index()
 axprev = plt.axes([0.7, 0.05, 0.1, 0.075])
@@ -104,8 +83,5 @@ bnext.on_clicked(callback.next)
 bprev = Button(axprev, 'Previous')
 bprev.on_clicked(callback.prev)
 
-
-
-
-
+print("Near the end")
 plt.show()
--- GUI_pcolormesh.py
+++ GUI_pcolormesh.py
@@ -1,24 +1,15 @@
 import lidar
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.colors as colors
-from matplotlib.colors import BoundaryNorm
-from matplotlib.ticker import MaxNLocator
 from matplotlib.widgets import Button
+from matplotlib import colors, ticker, cm
+from matplotlib import dates
 import matplotlib
-import matplotlib.tri as mtri
-from datetime import datetime, timedelta
 
 lidar_data=lidar.lidar('metoffice-lidar_faam_20150807_r0_B920_raw.nc')
 
-
-
-
-time_epoch = lidar_data['Time'][:].data
-epoch_list = time_epoch.tolist()
-datetime_list = [datetime.utcfromtimestamp(epoch_list[i]) for i in range(0, len(epoch_list))]
-datetime_array = np.array(datetime_list)
-
+m_time = dates.epoch2num(lidar_data['Time'][:].data)
+full_altitude = lidar_data['Altitude (m)'][:].data
 height_correction = 1.5 * np.arange(12148)
 
 def z_maker(x,y):
@@ -32,7 +23,14 @@ def height_maker(x,y,z):
     altitude_array = altitude_array.clip(0)
     return altitude_array
 
-
+def time_maker(x,y,z):
+    time = lidar_data['Time'][x:y].data
+    time_array = np.empty_like(z)
+    for j in range(0,len(z)):
+        for i in range(0, len(z[j])):
+            time_array[j,i] = time[i]
+    mpl_time = dates.epoch2num(time_array)
+    return mpl_time
 
 full_z = z_maker(0, len(lidar_data.profile[0][:].data[0]))
 
@@ -41,26 +39,57 @@ full_height = height_maker(0, len(lidar_data.profile[0][:].data[0]), full_z)
 def height_quick_maker(x,y):
     return full_height[:, x:y]
 
-
-
-
 fig, ax = plt.subplots()
 plt.subplots_adjust(bottom=0.2)
-
 start = 2000
 end = 2200
-z_test = z_maker(start, end)
-time_test = datetime_array[start:end]
-# time_base = datetime_array[start:end]
-# base_list = time_base.tolist()
-height_test = height_quick_maker(start, end)
-width = end - start
-c = plt.pcolormesh(time_test,height_test,z_test, norm=colors.LogNorm(vmin=0.000001, vmax=z_test.max()))
-print(c.get_array())
-print(z_test.max())
+z = z_maker(start, end)
+time = m_time[start:end]
+altitude = full_altitude[start:end]
+height = height_quick_maker(start, end)
+plt.ylim(0, altitude.max() * 1.1)
+plt.ylabel('Height (m)')
+plt.xlabel('time')
+c = plt.pcolormesh(time,height,z, norm=colors.LogNorm(vmin=0.000001, vmax=z.max()))
+d = plt.plot(time, altitude, color='black', linewidth=2)
+myFmt = dates.DateFormatter('%H:%M')
+ax.xaxis.set_major_formatter(myFmt)
 plt.colorbar(c)
 
+# fig, ax = plt.subplots()
+# plt.subplots_adjust(bottom=0.2)
+# start = 2000
+# end = 2200
+# z = z_maker(start, end)
+# time = time_maker(start, end, z)
+# time_flat = m_time[start:end]
+# altitude = full_altitude[start:end]
+# height = height_quick_maker(start, end)
+# plt.ylim(0, altitude.max() * 1.1)
+# plt.ylabel('Height (m)')
+# plt.xlabel('time')
+# c = plt.pcolormesh(time,height,z, norm=colors.LogNorm(vmin=0.000001, vmax=z.max()))
+# d = plt.plot(time_flat, altitude, color='black', linewidth=2)
+# myFmt = dates.DateFormatter('%H:%M')
+# ax.xaxis.set_major_formatter(myFmt)
+# plt.colorbar(c)
 
+# fig, ax = plt.subplots()
+# plt.subplots_adjust(bottom=0.2)
+# start = 2000
+# end = 2200
+# z = z_maker(start, end)
+# time = m_time[start:end]
+# altitude = full_altitude[start:end]
+# height = height_quick_maker(start, end)
+# plt.ylim(0, altitude.max() * 1.1)
+# plt.ylabel('Height (m)')
+# plt.xlabel('time')
+# contour_p = plt.pcolormesh(time,height,z, vmax=0.0007)
+# line_p = plt.plot(time, altitude, color='black', linewidth=2)
+# myFmt = dates.DateFormatter('%H:%M')
+# ax.xaxis.set_major_formatter(myFmt)
+# plt.colorbar(contour_p)
 
 class Index(object):
     def next(self, event):
@@ -78,9 +107,7 @@ class Index(object):
         # plt.draw()
 
     def prev(self, event):
-        print("pooop")
-
-
+        print("Testing testing 123")
 
 callback = Index()
 axprev = plt.axes([0.7, 0.05, 0.1, 0.075])
@@ -90,8 +117,5 @@ bnext.on_clicked(callback.next)
 bprev = Button(axprev, 'Previous')
 bprev.on_clicked(callback.prev)
 
-
-
-
-
+print("Near the end")
 plt.show()
